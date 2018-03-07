4dcbf8f262f66f1ca168e4bf343ecc530b7b30e9
Tomas Roy <hello@minicreative.net>
2018-03-07 16:45:21 -0500

    random background image implemented

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
-
 // Variables ===================================================================
 
 // Initialization ==============================================================
 
 // Run functions when document is ready
 $(document).ready(function () {
-
+	setCornerImage();
 });
 
 // Run function again on load & resize
 $(window).on('resize', function () {
 	
 });
+
+var setCornerImage = function () {
+	var i = Math.floor(Math.random() * Math.floor(5));
+	var cornerClass = "image-"+i;
+	$("#corner").addClass(cornerClass);
+};
