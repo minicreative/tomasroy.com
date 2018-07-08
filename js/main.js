2d3e8c95f4f2134c577c0676ff1a35f12ed66797
Tomas Roy <hello@minicreative.net>
2018-07-08 19:08:00 -0400

    analytics setup

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@
 // Run functions when document is ready
 $(document).ready(function () {
 	setCornerImage();
+	initializeGoogleAnalytics();
 });
 
 // Run function again on load & resize
@@ -16,4 +17,11 @@ var setCornerImage = function () {
 	var i = Math.floor(Math.random() * Math.floor(5));
 	var cornerClass = "image-"+i;
 	$("#corner").addClass(cornerClass);
+};
+
+var initializeGoogleAnalytics = function () {
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	gtag('config', 'UA-10868878-53');
 };
