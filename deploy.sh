46a6fe04b92f91049ff09a088cdb074f5ee3570e
Tomas Roy <hello@minicreative.net>
2021-08-12 10:14:54 -0500

    cleaner kube deployment, envsubst

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-yq eval ".spec.template.spec.containers[0].image = \"$1\"" deployment/deployment.yaml > deployment/deployment-merged.yaml
-kubectl apply -f deployment/deployment-merged.yaml
-kubectl apply -f deployment/service.yaml
-kubectl apply -f deployment/ingress.yaml
